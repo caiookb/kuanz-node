b003cb9582cd7adb9c0801069d14a29daf5c9f85
Caio <caiookb@gmail.com>
2020-04-18 16:53:16 -0300

    feature(SpendindController): Added updated spendings on post return

--- src/controllers/spending-controller.js
+++ src/controllers/spending-controller.js
@@ -1,9 +1,9 @@
 const Spending = require("../model/spending");
 const jwt = require("jsonwebtoken");
 
-const decoded = async req => {
+const decoded = async (req) => {
   const {
-    headers: { auth }
+    headers: { auth },
   } = req;
   const userDecoded = await jwt.decode(auth);
   return userDecoded.user;
@@ -14,16 +14,19 @@ module.exports = {
     const userDecoded = await decoded(req);
     req.body.userId = userDecoded._id;
     const spendings = await Spending.create(req.body);
-    return res
-      .status(201)
-      .send({ spendings, message: "Despesa cadastrada com sucesso!" });
+    const allSpendings = await Spending.find({ userId: userDecoded._id });
+    return res.status(201).send({
+      spendings,
+      allSpendings,
+      message: "Despesa cadastrada com sucesso!",
+    });
   },
   getSpendingsByUser: async (req, res) => {
     try {
       const userDecoded = await decoded(req);
       const spendings = await Spending.find({ userId: userDecoded._id });
       const totalSum = spendings
-        .map(spending => {
+        .map((spending) => {
           return spending.value;
         })
         .reduce((acc, current) => parseFloat(acc) + parseFloat(current));
@@ -38,11 +41,11 @@ module.exports = {
       const userDecoded = await decoded(req);
       const spendingToDelete = await Spending.findOne({
         _id: spending_id,
-        userId: userDecoded._id
+        userId: userDecoded._id,
       });
       const remove = await Spending.remove({
         _id: spendingToDelete._id,
-        userId: spendingToDelete.userId
+        userId: spendingToDelete.userId,
       });
       res.status(200).send({ remove, message: "Gasto deletado com sucesso!" });
     } catch (err) {
@@ -57,7 +60,7 @@ module.exports = {
       const update = await Spending.findByIdAndUpdate(
         {
           userId: userDecoded._id,
-          _id: _id
+          _id: _id,
         },
         { ...newValues },
         { useFindAndModify: true }
@@ -77,11 +80,11 @@ module.exports = {
 
       const spending = await Spending.findOne({
         userId: userDecoded._id,
-        _id: _id
+        _id: _id,
       });
       return res.status(200).send(spending);
     } catch (err) {
       res.status(500).send({ error: "Ocorreu algum erro na requisição" });
     }
-  }
+  },
 };
