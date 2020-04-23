f1826156c8c822f4e33bed967388778e9c939278
Caio <caiookb@gmail.com>
2020-04-23 18:24:02 -0300

    feature(Spendings): Added period to model

--- src/model/spending.js
+++ src/model/spending.js
@@ -4,45 +4,50 @@ const Schema = mongoose.Schema;
 const Spending = new Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   value: {
     type: String,
-    required: true
+    required: true,
   },
   type: {
     type: String,
-    required: true
+    required: true,
   },
   category: {
     type: String,
     required: true,
-    default: "spending"
+    default: "spending",
   },
   paid: {
     type: Boolean,
-    require: true
+    require: true,
   },
   paidDate: {
     type: Date,
-    required: true
+    required: true,
   },
   repeat: {
     type: Number,
     required: false,
-    default: 0
+    default: 0,
+  },
+  period: {
+    type: String,
+    required: false,
+    default: 0,
   },
   userId: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-Spending.pre("save", async function(next) {
+Spending.pre("save", async function (next) {
   return next();
 });
 
