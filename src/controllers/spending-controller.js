3a08588f8a860d4676da1ac33edf75f2d4bd06e6
Caio <caiookb@gmail.com>
2020-06-07 03:10:15 -0300

    feature(Spendings/Incomes): Added delete and update to installments transactions

--- src/controllers/spending-controller.js
+++ src/controllers/spending-controller.js
@@ -35,6 +35,12 @@ const getAllSpendingsAndSum = async (req) => {
   }
 };
 
+const getAllSpendings = async (req) => {
+  const userDecoded = await decoded(req);
+  const spendings = await Spending.find({ userId: userDecoded._id });
+  return spendings;
+};
+
 module.exports = {
   createSpending: async (req, res) => {
     try {
@@ -43,11 +49,23 @@ module.exports = {
       req.body.userId = userDecoded._id;
 
       if (repeat >= 2) {
+        let spendingFixedId = undefined;
+
         for (let i = 0; i < repeat; i++) {
           const futureDate = moment(paidDate)
             .add(i, period)
             .format("YYYY-MM-DD");
           req.body.paidDate = futureDate;
+          req.body.repeated = true;
+
+          if (i == 0) {
+            await Spending.create(req.body).then((firstSpending) => {
+              spendingFixedId = firstSpending._id;
+            });
+          } else {
+            req.body.installmentId = spendingFixedId.toString();
+            await Spending.create(req.body);
+          }
           await Spending.create(req.body);
         }
       } else {
@@ -57,7 +75,7 @@ module.exports = {
       const SpendingsAndSum = await getAllSpendingsAndSum(req);
       return res.status(201).send({
         ...SpendingsAndSum,
-        message: "Despesa cadastrada com sucesso!",
+        message: "Despesa cadastrada com sucesssssso!",
       });
     } catch (err) {
       res.status(500).send({ error: "Ocorreu algum erro na requisição" });
@@ -73,21 +91,47 @@ module.exports = {
   },
   deleteSpendingById: async (req, res) => {
     try {
-      const { spending_id } = req.body;
+      const { spending_id, type, installmentId } = req.body;
       const userDecoded = await decoded(req);
-      const spendingToDelete = await Spending.findOne({
+      const actualSpending = await Spending.findOne({
         _id: spending_id,
         userId: userDecoded._id,
       });
-      const remove = await Spending.remove({
-        _id: spendingToDelete._id,
-        userId: spendingToDelete.userId,
-      });
-      const SpendingsAndSum = await getAllSpendingsAndSum(req);
+      const actualDate = moment(actualSpending.receiveDate);
+      const allSpending = await getAllSpendings(req);
+      const futureSpending = allSpending.filter(
+        (Spending) => moment(Spending.receiveDate) > actualDate
+      );
+      if (type === "ACTUAL") {
+        await Spending.remove({
+          _id: spending_id,
+          userId: userDecoded._id,
+        });
+      } else if (type === "ACTUAL_AND_NEXTS") {
+        for (let i = 0; i < futureSpending.length; i++) {
+          await Spending.remove({
+            _id: futureSpending[i]._id,
+            userId: userDecoded._id,
+          });
+        }
+      } else if (type === "EVERY") {
+        const installmentSpending = allSpending.filter((Spending) => {
+          return (
+            Spending.id == installmentId ||
+            Spending.installmentId == installmentId
+          );
+        });
+        for (let i = 0; i < installmentSpending.length; i++) {
+          await Spending.remove({
+            _id: installmentSpending[i]._id,
+            userId: userDecoded._id,
+          });
+        }
+      }
+      const SpendingAndSum = await getAllSpendingsAndSum(req);
       res.status(200).send({
-        remove,
-        ...SpendingsAndSum,
-        message: "Gasto deletado com sucesso!",
+        ...SpendingAndSum,
+        message: "deletado com sucesso!",
       });
     } catch (err) {
       res.status(500).send({ error: "Ocorreu algum erro na requisição" });
@@ -95,17 +139,87 @@ module.exports = {
   },
   updateSpendingById: async (req, res) => {
     try {
-      const { _id } = req.body;
+      const { spending_id, type, installmentId } = req.body;
       const newValues = req.body;
       const userDecoded = await decoded(req);
-      const update = await Spending.findByIdAndUpdate(
-        {
-          userId: userDecoded._id,
-          _id: _id,
-        },
-        { ...newValues },
-        { useFindAndModify: true }
-      );
+
+      const actualSpending = await Spending.findOne({
+        _id: spending_id,
+        userId: userDecoded._id,
+      });
+      const actualDate = moment(actualSpending.paidDate);
+      const allSpendings = await getAllSpendings(req);
+
+      const filteredSpendings = allSpendings.filter((spendings) => {
+        return (
+          moment(spendings.paidDate) >= actualDate &&
+          (spendings.id == installmentId ||
+            spendings.installmentId == installmentId)
+        );
+      });
+
+      filteredSpendings.map((mappedSpendings, i) => {
+        mappedSpendings.name = newValues.name;
+        mappedSpendings.value = newValues.value;
+        mappedSpendings.type = newValues.type;
+        // mappedSpendings.receiveDate = moment(newValues.receiveDate).add(
+        //   i,
+        //   period
+        // );
+      });
+
+      if (type === "ACTUAL") {
+        await Spending.findOneAndUpdate(
+          {
+            _id: spending_id,
+            userId: userDecoded._id,
+          },
+          { ...newValues },
+          { useFindAndModify: true }
+        );
+      } else if (type === "ACTUAL_AND_NEXTS") {
+        console.log("Future Spending", filteredSpendings);
+
+        for (let i = 0; i < filteredSpendings.length; i++) {
+          await Spending.findOneAndUpdate(
+            {
+              _id: filteredSpendings[i]._id,
+              userId: userDecoded._id,
+            },
+            { ...filteredSpendings[i]._doc },
+            { useFindAndModify: true }
+          );
+        }
+      } else if (type === "EVERY") {
+        const installmentSpendings = allSpendings.filter((spending) => {
+          return (
+            spending.id == installmentId ||
+            spending.installmentId == installmentId
+          );
+        });
+
+        installmentSpendings.map((mappedSpendings, i) => {
+          mappedSpendings.name = newValues.name;
+          mappedSpendings.value = newValues.value;
+          mappedSpendings.type = newValues.type;
+          // mappedSpendings.receiveDate = moment(newValues.receiveDate).add(
+          //   i,
+          //   period
+          // );
+        });
+
+        for (let i = 0; i < installmentSpendings.length; i++) {
+          await Spending.findByIdAndUpdate(
+            {
+              _id: installmentSpendings[i]._id,
+              userId: userDecoded._id,
+            },
+            { ...installmentSpendings[i]._doc },
+            { useFindAndModify: true }
+          );
+        }
+      }
+
       const SpendingsAndSum = await getAllSpendingsAndSum(req);
       res.status(200).send({
         update,
